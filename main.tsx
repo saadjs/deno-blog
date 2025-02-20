6209c26a30f9c9729e61988ffaffd5915cb97034
Saad Bash <saadbash08@gmail.com>
2025-02-20 09:49:46 -0500

    chore(main.tsx): update author email

--- main.tsx
+++ main.tsx
@@ -10,7 +10,7 @@ blog({
     favicon: "./assets/favicon.ico",
     avatar: "./assets/avatar.png",
     links: [
-        { title: "Email", url: "mailto:saad@saad.sh" },
+        { title: "Email", url: "mailto:saadbashdev@gmail.com" },
         { title: "GitHub", url: "https://github.com/saadjs" },
         { title: "Linkedin", url: "https://www.linkedin.com/in/saadbash/" },
     ],
